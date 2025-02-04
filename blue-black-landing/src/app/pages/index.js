e6ac498642a2fd9c56b877fe5a7e57443afc437a
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2025-02-04 21:23:50 +0530

    Update index.js

--- blue-black-landing/src/app/pages/index.js
+++ blue-black-landing/src/app/pages/index.js
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <Header />
+      <Header />   
       <Hero />
       <About />
       <Projects />
